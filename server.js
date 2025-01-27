baa2e9d196b9c31101ef6f7642657e217bccefff
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-01-27 18:42:27 +0100

    final fixes + http error when visiting non existing sites & tests

--- server.js
+++ server.js
@@ -1,48 +1,6 @@
-import express from "express";
-import expressLayouts from "express-ejs-layouts";
-import { getMoviesFromAPI, getMovieFromId } from './static/js/movieRetriever.js';
-
-const app = express();
+import { app } from "./static/js/app.js";
 
 const PORT = 5080;
-
-app.use("/static", express.static("./static"));
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-
-app.get("", (req, res) => {
-    res.render("index");
-})
-
-app.get("/about", (req, res) => {
-    res.render("about");
-})
-
-app.get("/cafe", (req, res) => {
-    res.render("cafe");
-})
-
-app.get("/contact", (req, res) => {
-    res.render("contact");
-})
-
-app.get("/movies", async (req, res) => {
-    let loadedMovies = await getMoviesFromAPI();
-
-    res.render("movies", {movies: loadedMovies.data});
-})
-
-
-import MarkdownIt from "markdown-it";
-const md = MarkdownIt();
-
-app.get("/movie/:id", async (req, res) => {
-    let loadedMovie = await getMovieFromId(req.params.id);
-
-    let introTextHTML = md.render(loadedMovie.data.attributes.intro);
-
-    res.render("movie", {movie: loadedMovie.data, markedIntroText: introTextHTML});
-})
 
 app.listen(PORT, () => {
     console.log("Server is up at: http://localhost:5080");
